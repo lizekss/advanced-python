67710f1b7550b9d3222459bd1a5c6a7787b46363
lizekss <lekse20@freeuni.edu.ge>
2024-10-03 18:54:19 +0400

    add data analysis assignment

---
+++ data-analysis/README.md
@@ -0,0 +1,17 @@
+# Data Analysis with pandas
+This project is designed to analyze student performance data across multiple semesters and subjects. The data is stored in a CSV file and processed using the `pandas` library in Python. The analysis includes identifying students with scores below 50, calculating average scores for each subject in each semester, finding the students with the highest average scores, determining the hardest subject, and creating a new dataframe with average scores per semester.
+## Getting Started
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes.
+### Prerequisites
+This project uses the following Python libraries:
+- pandas
+- numpy
+- matplotlib
+You can install these packages using pip:
+
+`pip install requirements.txt`
+
+### Usage
+After installing the requirements, run the main script, which performs various analyses on the data and displays the bar and line charts for visualization.
+
+`python main.py`
---
+++ data-analysis/main.py
@@ -0,0 +1,43 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+df = pd.read_csv('student_scores_random_names.csv')
+# df.dropna()
+
+print(df.head())
+
+subjects = ['Math', 'Physics', 'Chemistry', 'Biology', 'English']
+failed_students = df[df[subjects].lt(50).any(axis=1)]['Student'].unique()
+print(failed_students)
+
+average_semester_scores = df.groupby('Semester')[subjects].mean()
+print(average_semester_scores)
+
+df['Average_Score'] = df[subjects].mean(axis=1)
+highest_average_score_student = df.groupby('Student')['Average_Score'].mean().idxmax()
+print(highest_average_score_student)
+
+average_subject_scores = df[subjects].apply(np.mean)
+hardest_subject = average_subject_scores.idxmin()
+print(hardest_subject)
+
+
+average_semester_scores.to_excel('average_scores_per_semester.xlsx', index=False)
+
+average_semester_scores.plot(kind='bar', figsize=(10, 7))
+plt.title('Average Scores by Subject for All Semesters')
+plt.xlabel('Semester')
+plt.ylabel('Average Score')
+plt.legend(title='Subjects', loc='upper left')
+
+df['Total_Score'] = df[subjects].sum(axis=1)
+average_total_scores = df.groupby('Semester')['Total_Score'].mean()
+
+plt.figure()
+average_total_scores.plot(kind='line', figsize=(10, 7), marker='o')
+plt.title('Average Total Score per Semester')
+plt.xlabel('Semester')
+plt.ylabel('Average Total Score')
+plt.grid(True)
+plt.show()
